9b8a868d8fb69bc9e09bf232daf62ffba3cd2c32
hascode <hascode@localhost>
2012-10-21 19:13:03 +0200

    Initial import. Examples using nodejs and jetty added.

---
+++ README.md
@@ -0,0 +1,34 @@
+# HTML5 Server Send Events using Node.js or Jetty
+
+A short example for the W3C working draft implementation on server side events.
+
+You're able to run the examples using *node.js* or *Maven* with Jetty:
+
+## Node.js
+
+Simply run the following command:
+
+    node server.js
+
+Afterwards just open the following URL in your browser: [http://localhost:8080/]
+
+
+## Maven + Jetty
+
+You need to have [Maven] installed - then simply run the following goal:
+
+    mvn jetty:run
+
+Again you're able to see the results when opening [http://localhost:8080/] in your browser.
+
+## Tutorial
+
+Please feel free to take a look at the full tutorial on my blog at [www.hascode.com]. 
+
+---
+
+**2012 Micha Kops / hasCode.com**
+
+  [http://localhost:8080/]:http://localhost:8080/
+  [Maven]:http://maven.apache.org
+  [www.hascode.com]:http://www.hascode.com/
---
+++ src/main/java/com/hascode/tutorial/MySSEServlet.java
@@ -0,0 +1,42 @@
+package com.hascode.tutorial;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import org.eclipse.jetty.servlets.EventSource;
+import org.eclipse.jetty.servlets.EventSourceServlet;
+
+@WebServlet(urlPatterns = "/talk", initParams = { @WebInitParam(name = "heartBeatPeriod", value = "5") }, asyncSupported = true)
+public class MySSEServlet extends EventSourceServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected EventSource newEventSource(final HttpServletRequest req) {
+		return new EventSource() {
+
+			@Override
+			public void onOpen(final Emitter emitter) throws IOException {
+				emitter.data("new server event " + new Date().toString());
+				while (true) {
+					System.out.println("propagating event..");
+					try {
+						Thread.sleep(5000);
+						emitter.data("new server event "
+								+ new Date().toString());
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			@Override
+			public void onClose() {
+				System.out.println("closed");
+			}
+		};
+	}
+}
